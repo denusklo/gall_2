7ce5f6681ee951b88d417b0d9dce15d6e85e4316
Kong Chak Sung <denusklo@gmail.com>
2023-07-27 22:16:42 +0800

    commit

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('login', function () {
+    return redirect()->route('user.login.form');
+});
+
 Route::prefix('v1')->group(function () {
     
     Route::get('test', [HomeController::class, 'api'])->name('api.test');
