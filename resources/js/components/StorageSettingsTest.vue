5cfee49ddc2c2fc0ece5e041e08ef9d645c9a1ef
Denusklo <denusklo@gmail.com>
2026-02-07 14:03:00 +0800

    feat: add storage settings test component
    - Add StorageSettingsTest.vue for testing storage credential functionality
    - Update storageSettingsApp.js to register test component

---
+++ resources/js/components/StorageSettingsTest.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container py-4">
+    <h1>Storage Settings Test</h1>
+    <p>If you see this, Vue is working!</p>
+  </div>
+</template>
+
+<script setup>
+console.log('Test component mounted!');
+</script>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
